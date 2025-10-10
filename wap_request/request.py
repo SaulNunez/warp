8148545838edfbf3ae303adc946d95b3bbca44b6
Saul Nunez <saul.nunez99@gmail.com>
2025-10-10 10:46:11 -0600

    WIP (TUI for browser)

--- wap_request/request.py
+++
@@ -1,4 +0,0 @@
-_headers = {'Accept', 'text/vnd.wap.wml'}
-
-def request_wap(path: str):
-    return requests.get(path, headers=_headers)
---
+++ warp.py
@@ -0,0 +1,4 @@
+from wap_request.wap_request import request_wap
+from warp_tui.warp_tui import BrowserApp
+
+BrowserApp().run()
